527d5a258fe6c068ff7d959b34161c4f818ec256
lzg <398055960@qq.com>
2019-10-29 17:26:04 +0800

    update

--- src/components/dialog/src/main.vue
+++ src/components/dialog/src/main.vue
@@ -1,3 +1,75 @@
+<template>
+  <div class="dialog-mask" v-if="showDialog">
+    <div class="dialog-box">
+      <header class="header" v-html="title" v-if="title" :class="{'input': type === 'input'}">
+      </header>
+      <div class="body tc" v-html="text" v-if="type === 'text'">
+      </div>
+      <div class="edit-block" v-if="type === 'textarea'">
+        <textarea class="edit-el" rows="1" v-model="text" autofocus id="tex" :placeholder='ptext'></textarea>
+      </div>
+      <div class="selectList" v-if="type === 'select'">
+        <div class="listItem" v-for="(item,index) in list" :key="index" @click="toSelect(item)">
+          <span :class="{'active':item.isSelect}">{{item.val}}</span>
+          <img src="../../../assets/img/gou.png" alt="" width="16" class="fr" v-if="item.isSelect">
+        </div>
+      </div>
+      <div class="footer tc">
+        <button class="my-cancel-btn" v-show="showCancelBtn" v-text="cancelText" @click="cancelClick"></button>
+        <button class="confirm-btn" :style="{width: showCancelBtn ? '50%' : '100%', color: theme}" v-text="confirmText" @click="confirmClick"></button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'dialog-box',
+  data () {
+    return {
+      showDialog: true,
+      type: 'text',
+      title: '',
+      text: '',
+      ptext: '',
+      showCancelBtn: true,
+      confirmText: '确定',
+      cancelText: '取消',
+      placeholder: '',
+      theme: '#F64C4C',
+      confirm: null,
+      list: []
+    }
+  },
+  methods: {
+    cancelClick () {
+      this.showDialog = false
+    },
+    confirmClick () {
+      if (this.type === 'select' && !this.text) {
+        return
+      }
+      this.confirm(this.text)
+      this.showDialog = false
+    },
+    toSelect (item) {
+      for (var i in this.list) {
+        this.$set(this.list[i], 'isSelect', false)
+      }
+      this.$set(item, 'isSelect', true)
+      this.text = item
+    }
+  },
+  mounted () {
+    this.text = this.content
+    if (this.type === 'textarea') {
+      this.$nextTick(() => {
+        document.getElementById('tex').focus()
+      })
+    }
+  }
+}
+</script>
+
 <style lang="less" scoped>
 .dialog-mask {
   position: fixed;
@@ -81,82 +153,9 @@ textarea {
       right: 30px;
       top: 13px;
     }
-    .active{
-      color: #40A9FF;
+    .active {
+      color: #40a9ff;
     }
   }
 }
 </style>
-<template>
-  <div class="dialog-mask" v-if="showDialog">
-    <div class="dialog-box">
-      <header class="header" v-html="title" v-if="title" :class="{'input': type === 'input'}">
-      </header>
-      <article class="body tc" v-html="text" v-if="type === 'text'">
-      </article>
-      <article class="edit-block" v-if="type === 'textarea'">
-        <textarea class="edit-el" rows="1" v-model="text" autofocus id="tex" :placeholder='ptext'></textarea>
-      </article>
-      <article class="selectList" v-if="type === 'select'">
-        <div class="listItem" v-for="(item,index) in list" :key="index" @click="toSelect(item)">
-          <span :class="{'active':item.isSelect}">{{item.val}}</span>
-          <img src="../../../assets/img/gou.png" alt="" width="16" class="fr" v-if="item.isSelect">
-        </div>
-      </article>
-      <div class="footer tc">
-        <button class="my-cancel-btn" v-show="showCancelBtn" v-text="cancelText" @click="cancelClick"></button>
-        <button class="confirm-btn" :style="{width: showCancelBtn ? '50%' : '100%', color: theme}" v-text="confirmText" @click="confirmClick"></button>
-      </div>
-    </div>
-  </div>
-</template>
-<script>
-export default {
-  name: 'dialog-box',
-  data () {
-    return {
-      showDialog: true,
-      type: 'text',
-      title: '',
-      text: '',
-      ptext: '',
-      showCancelBtn: true,
-      confirmText: '确定',
-      cancelText: '取消',
-      placeholder: '',
-      theme: '#F64C4C',
-      confirm: null,
-      list: []
-    }
-  },
-  methods: {
-    cancelClick () {
-      this.showDialog = false
-    },
-    confirmClick () {
-      if (this.type === 'select' && !this.text) {
-        return
-      }
-      this.confirm(this.text)
-      this.showDialog = false
-    },
-    toSelect (item) {
-      for (var i in this.list) {
-        this.$set(this.list[i], 'isSelect', false)
-      }
-      this.$set(item, 'isSelect', true)
-      this.text = item
-      // this.confirm(this.text)
-      // this.showDialog = false
-    }
-  },
-  mounted () {
-    this.text = this.content
-    if (this.type === 'textarea') {
-      this.$nextTick(() => {
-        document.getElementById('tex').focus()
-      })
-    }
-  }
-}
-</script>
